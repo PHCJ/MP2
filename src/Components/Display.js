9487895e6d745b8dd9f817085f6b5616d9b9b974
Paulo <paulo.jesus2@fatec.sp.gov.br>
2023-06-16 21:48:10 -0300

    IOS e Android

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,9 +1,24 @@
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
-export default function Display({ value = '0', tFont = 50, tFlex = 1 }) {
+export default function Display({value="0", tFont = 50, tFlex = 1}) {
   return (
     <View style={styles(tFlex).container}>
-      <Text style={styles(tFont).content}>{String(value)}</Text>
+      <Text style={styles(tFont).content}>{(String(value))}</Text>
     </View>
-  );
+  )
 }
+
+const styles =(tFont,tFlex)=> StyleSheet.create({
+  container: {
+    flex: tFlex,
+    backgroundColor: "#ccc",
+    borderWidth: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    marginHorizontal:20,
+  },
+  content: {
+    fontSize: tFont,
+    padding: 10,
+  },
+});
